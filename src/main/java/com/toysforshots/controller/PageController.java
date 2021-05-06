6ae1530c851d0e9e5bac902b7cde15d8ea8df2a0
deashadow <67285924+deashadow@users.noreply.github.com>
2021-05-06 12:49:03 -0400

    Spring Boot basic project set up

---
+++ src/main/java/com/toysforshots/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.toysforshots.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Controller //@RestController will override the index.html file
+//public class PageController {
+
+    //@RequestMapping("/")
+   // public String home() {
+    //    return "HelloWorld";
+   // }
+//}
